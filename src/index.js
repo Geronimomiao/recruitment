2fb934b592d1dca16c15b8cc384e51e7e23be492
wsm <1030057982@qq.com>
2019-05-14 06:53:38 +0800

    初步成型

--- src/index.js
+++ src/index.js
@@ -1,43 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link, BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect, Link} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import Dashboard from './Dashboard';
 import Auth from './Auth';
 import * as serviceWorker from './serviceWorker';
 
-const Two = () => {
-  return <h2>222222</h2>
-}
-
-const Three = () => {
-  return <h2>33333</h2>
-}
-
-class Test extends React.Component{
-  constructor(props){
-    super(props)
-  }
-  render(){
-    console.log(this.props)
-    return <h2>{this.props.match.params.location}</h2>
-  }
-}
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Route path='/' exact component={App}></Route>
-      <Route path='/dashboard'  component={Dashboard}></Route>
+  <BrowserRouter>
+    <ul>
+      <li>
+        <Link to='/auth'>auth</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>dashboard</Link>
+      </li>
+    </ul>
+    <Switch>
+      <Route path='/dashboard' component={Dashboard}></Route>
       <Route path='/auth' component={Auth}></Route>
-      <Route path='/three' component={Three}></Route>
-    </BrowserRouter>,
+      <Redirect to='/dashboard'></Redirect>
+      {/* Switch 只渲染一个路由 如果上面都没匹配到 会重定向至 '/dashboard' */}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-
-
-
 
 
 // If you want your app to work offline and load faster, you can change
